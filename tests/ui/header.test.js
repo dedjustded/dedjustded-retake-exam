6dbda26f3d6f8cc3e940a8e3853edf459624e575
Кирил Бърдаров <dedjustded@gmail.com>
2024-04-19 09:38:39 +0300

    fixed tests

--- tests/ui/header.test.js
+++ tests/ui/header.test.js
@@ -1,9 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check header', async ({ page }) => {
-    await page.goto('http://localhost:8080'); 
+    await page.goto('http://localhost:8081'); 
     const homeLink = await page.$('a[href="/"]');
     const text = await homeLink.textContent();
-    expect(text).toBe('Games Collection');
+    expect(text).toBe('Home');
   });
   
